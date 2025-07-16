9281461ca03175f55e86021608a15a80c618d466
hoangdesu <hoangdesu@gmail.com>
2025-07-16 15:38:11 +0700

    Completed tic tac toe game, Linear search algorithm

--- jul15_tictactoe.py
+++ jul15_tictactoe.py
@@ -42,34 +42,90 @@ for i in range(3):
         count += 1
     board.append(row)
         
-print(board)
-        
-        
+# print(board)
+
+# starting player is always x
+player = 'x'
+
+game_continue = True
+
+print()
+for row in board:
+    for num in row:
+        print(num, end=" ")
+    print()
 
 # game loop
-while True:
-    # 1. display the board
+while game_continue:
+
+    # 1. get user input
+    move = int(input(f'\nplayer {player} move: '))
+    
+    
+    # 2. check valid move
+    while move < 1 or move > 9:
+        move = int(input(f'{move} is an invalid move. Please enter a number 1-9: '))
+            
+ 
+    # 3. record move into the board
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == move:
+                board[i][j] = player
+    
+    
+    # 4. check win condition
+    # 1 2 3 or 456 or 789
+    
+    # horizontal match -> check all rows equal
+    # row[i][0] == row[i][1] == row[i][2]
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            print(f'Player {player} wins!')
+            game_continue = False
+        # for 
+
+    
+    # # vertical match -> check all columns equal
+    # row[0][i] == row[1][i] == row[2][i]
+
+    for i in range(len(board[0])):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            print(f'Player {player} wins!')
+            game_continue = False
+    
+    
+    # diagonal match        
+    # board[0][0] == board[1][1] == board[2][2] or board[2][0] == board[1][1] == board[0][2]
+    # for i in range(len(board)):
+    #     for j in range(len(board[i])):
+    #         if i == j: ...
+
+    # if left to right or right to left match
+    if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) or (board[2][0] == board[1][1] and board[1][1] == board[0][2]):
+        print(f'Player {player} wins!')
+        game_continue = False
+
+    
+    
+    # 5. display the updated board
     print()
     for row in board:
         for num in row:
             print(num, end=" ")
         print()
+        
+        
+    # 6. toggle the player after 1 round
+    if player == 'x':
+        player = 'o'
+    elif player == 'o':
+        player = 'x'
     
     
-    # 2. get user input
-    move = int(input('x move: '))
-    
-    
-    # 3. check valid move
-    while move < 1 or move > 9:
-        move = int(input(f'{move} is an invalid move. Please enter a number 1-9: '))
-    
-    
-    # 4. record move into the board
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == move:
-                board[i][j] = 'x'
-                
-    
-    # 5. check win condition
+print('Thanks for playing ^o^')
+
+# Tech debt
+# Software Developement Cycle => Dont go for perfection
+
+# Naive approach vs Optimal approach
---
+++ jul16_linear_search.py
@@ -0,0 +1,49 @@
+# Linear search: tìm kiếm tuyến tính
+
+# menu = ['pho', 'com tam', 'bun mam', 'bun cha']
+
+# order = input('what u want to order: ') # pho
+
+# # flag: đặt cờ
+# found = False
+
+# for i in range(len(menu)):
+#     # if order == menu[i]:
+#     #     print(f'your {order} is on the way!')
+#     # else:
+#     #     print(f'your {order} is not in our menu')
+    
+#     if order == menu[i]:
+#         found = True
+#         break # ignore the rest of the list
+        
+
+# # conclude after finish searching
+# if found:
+#     print(f'your {order} is on the way!')
+# else:
+#     print(f'sorry, {order} is not in our menu :(')
+
+
+# # ensure the element exists before finding the index
+# print(menu.index('pizza'))
+
+
+Challenge:
+1. give me a list of 10 random numbers
+
+
+2. allow user to enter an int
+
+
+3. if found: tell the user WHERE (what index) that number appears in the list
+if not found, let the user know
+
+[3, 2, 9, 5, 4]
+
+5
+5 appears at index 3 in the list
+
+8
+8 appears not to be in the list
+
